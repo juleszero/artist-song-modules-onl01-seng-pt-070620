9169ec14d8c6a6867d95394f141651945613ee5b
Juliana Connors <juliana.g.connors@gmail.com>
2020-08-02 21:00:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,17 @@ require_relative '../lib/concerns/memorable'
 require 'pry'
 
 class Song
-  extend Memorable::ClassMethods
+  extend Memorable::ClassMethods, Findable
   include Memorable::InstanceMethods, Paramable
+  
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
